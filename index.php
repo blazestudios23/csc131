f46f97ab94a6f49cfbc6b1e094e90711f984978f
Bhushan Ladde <Bhushan Ladde>
2016-06-09 21:34:50 -0700

    Javascript Code - To add new row in a table

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="styles.css">
+<link rel="stylesheet" type="text/css" href="style/styles.css">
 <?php
-include '/functions/functions.php';
+include 'functions/functions.php';
 ?>
-<script src="/functions/functions.js"></script>
+<script src="functions/functions.js"></script>
 </head>
 <body>
 <header>
@@ -13,11 +13,24 @@ include '/functions/functions.php';
 
 </header>
 <article>
+ 
+  <table id="studentDetails">
+  <tr>
+    <td>Row1 cell1</td>
+    <td>Row1 cell2</td>
+  </tr>
+  <tr>
+    <td>Row2 cell1</td>
+    <td>Row2 cell2</td>
+  </tr>
+  <tr>
+    <td>Row3 cell1</td>
+    <td>Row3 cell2</td>
+  </tr>
+</table>
+<br>
 
-  <table>
-
-
-  </table>
+<button onclick="insertRows()">Add row</button>
 </article>
 
 <footer>
