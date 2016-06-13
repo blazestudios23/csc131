aeebabaeaae4710533b48bd425bccad08589924a
Andrew Origewitsch <blazestudios23@gmail.com>
2016-06-12 19:50:53 -0700

    messing with login

--- functions/functions.php
+++ functions/functions.php
@@ -1,5 +1,11 @@
 <?php
+session_start();
+   unset($_SESSION["username"]);
+   unset($_SESSION["password"]);
 
+   if(!isset($_SESSION['login'])){ //if login in session is not set
+       header("Location: login.php");
+   }
 
 
  ?>
--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <html>
 <head>
-  <title>Student Attencance</title>
-  <link rel="stylesheet" type="text/css" href="styles.css">
-  <link href = "css/bootstrap.min.css" rel = "stylesheet">
+
   <?php
-    include 'functions/functions.php';
-    ob_start();
-    session_start()
+  include 'head.php';
   ?>
-  <script src="functions/functions.js"></script>
+
+  <title>Student Attencance</title>
 </head>
 <body>
   <header>
